16a4ccf53ab8d5d43a79adbc8a4d2586a76ccd99
alchemist123 <42992006+alchemist123@users.noreply.github.com>
2021-03-08 11:52:07 +0530

    base set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <Container>
+      <div className="LoginBox">
+      <p>
+      Technology is changing every day. Every day, there are new enhancements, every day there is a new invention.
+      We have to keep our self very active and updated otherwise we will be left behind in the race. 
+      We have to learn the new thing every day. We should learn to adapt with new things.
+      Standing rigid with the old things will make us lose the competition.
+      </p>
+      </div>
+      </Container>
     </div>
   );
 }
