57d61168146d844b4a369ad541266677b570c4fc
alchemist123 <42992006+alchemist123@users.noreply.github.com>
2021-03-09 13:40:00 +0530

    cards added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import './App.css';
 function App() {
   return (
     <div className="App-header"><br/>
-      <img src={pro} style={{borderRadius:"50%", width:"10%",height:"10%", opacity:"90%"}}/><br/>
+      <img src={pro} style={{borderRadius:"50%", width:"10%",height:"10%", opacity:"90%"}}/>
+      AMAL V S<br/>
       <table>
         <tr>
           <a href="https://www.linkedin.com/in/amal-v-s-948079200/"><th><img src={ln} style={{width:"35px",height:"35px"}}/></th></a>&nbsp;&nbsp;&nbsp;
@@ -26,31 +27,61 @@ function App() {
       Standing rigid with the old things will make us lose the competition.</b>
       </p><br/>
       <table>
-        <tr>
+        <tr>&nbsp;
           <td>
-                  <Card style={{ width: '13rem'}}>
-                    <Card.Body>
-                    <Card.Title className="carfont">PROJECT</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                  <Card style={{ width: '13rem',backgroundColor:"grey",borderRadius:"10%"}}>
+                    <Card.Body><br/>
+                    <Card.Title className="carfont">Projects</Card.Title>
                     <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
+                      <ul>
+                        <li>Doc Online</li>
+                        <li>Inji - film review</li>
+                        <li>heya-chat app</li>
+                        <li>covid-19 tacking </li>
+                      </ul>
+                       
                 </Card.Text>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
+                <Card.Link href="https://github.com/alchemist123?tab=repositories" style={{fontSize:"17px", color:"white"}}>source code...</Card.Link>
                 </Card.Body>
                 </Card></td>
                 <td>
-                  <Card style={{ width: '13rem' }}>
-                    <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                <Card style={{ width: '13rem',backgroundColor:"grey",borderRadius:"10%"}}>
+                    <Card.Body><br/>
+                    <Card.Title className="carfont">Research</Card.Title>
                     <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
+                       Real-time humen emotion detection using CNN and fer2013 dataset.
+                       (Artificial Intelligence)
                 </Card.Text>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
+                <Card.Link href="https://github.com/alchemist123" style={{fontSize:"17px", color:"white"}}>source code...</Card.Link>
+                </Card.Body>
+                </Card></td>
+        </tr>
+        <tr>&nbsp;
+          <td>
+                  <Card style={{ width: '13rem',backgroundColor:"grey",borderRadius:"10%"}}>
+                    <Card.Body><br/>
+                    <Card.Title className="carfont">Projects</Card.Title>
+                    <Card.Text>
+                      <ul>
+                        <li>Doc Online</li>
+                        <li>Inji - film review</li>
+                        <li>heya-chat app</li>
+                        <li>covid-19 tacking </li>
+                      </ul>
+                       
+                </Card.Text>
+                <Card.Link href="https://github.com/alchemist123?tab=repositories" style={{fontSize:"17px", color:"white"}}>source code...</Card.Link>
+                </Card.Body>
+                </Card></td>
+                <td>
+                <Card style={{ width: '13rem',backgroundColor:"grey",borderRadius:"10%"}}>
+                    <Card.Body><br/>
+                    <Card.Title className="carfont">Research</Card.Title>
+                    <Card.Text>
+                       Real-time humen emotion detection using CNN and fer2013 dataset.
+                       (Artificial Intelligence)
+                </Card.Text>
+                <Card.Link href="https://github.com/alchemist123" style={{fontSize:"17px", color:"white"}}>source code...</Card.Link>
                 </Card.Body>
                 </Card></td>
         </tr>
