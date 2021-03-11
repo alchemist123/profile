beade7841d6e6c6a8f7992f6a58287ca46d2eb83
alchemist123 <42992006+alchemist123@users.noreply.github.com>
2021-03-11 11:20:35 +0530

    make cards responsive

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import insta from './insta.gif'
 import './App.css';
 function App() {
   return (
-    <div className="App-header"><br/>
+
+    <div className="App-header">
+
+    <br/>
       <img src={pro} style={{borderRadius:"50%", width:"10%",height:"10%", opacity:"90%"}}/>
       AMAL V S<br/>
       <table>
@@ -26,72 +29,58 @@ function App() {
       We have to learn the new thing every day. We should learn to adapt with new things.
       Standing rigid with the old things will make us lose the competition.</b>
       </p><br/>
-      <table>
-        <tr>&nbsp;
-          <td>
-                  <Card style={{ width: '13rem',backgroundColor:"grey",borderRadius:"10%"}}>
-                    <Card.Body><br/>
-                    <Card.Title className="carfont">Projects</Card.Title>
-                    <Card.Text>
-                      <ul>
+              <div className="row">
+                <div className="column">
+                  <div className="card">
+                    <h6 className="carfont">project</h6>
+                    <p>
+                    <ul style={{textAlign:"left"}}>
                         <li>Doc Online</li>
                         <li>Inji - film review</li>
                         <li>heya-chat app</li>
                         <li>covid-19 tacking </li>
                       </ul>
-                       
-                </Card.Text>
-                <Card.Link href="https://github.com/alchemist123?tab=repositories" style={{fontSize:"17px", color:"white"}}>source code...</Card.Link>
-                </Card.Body>
-                </Card></td>
-                <td>
-                <Card style={{ width: '13rem',backgroundColor:"grey",borderRadius:"10%"}}>
-                    <Card.Body><br/>
-                    <Card.Title className="carfont">Research</Card.Title>
-                    <Card.Text>
-                       Real-time humen emotion detection using CNN and fer2013 dataset.
+                      </p>
+                  </div>
+                 
+                </div>
+                <div className="column">
+                <div className="card">
+                    <h6 className="carfont">Research</h6>
+                    <p>
+                    Real-time humen emotion detection using CNN and fer2013 dataset.
                        (Artificial Intelligence)
-                </Card.Text>
-                <Card.Link href='/Ml' style={{fontSize:"17px", color:"white"}}>Test real-time...</Card.Link>
-                </Card.Body>
-                </Card></td>
-        </tr>
-        <tr>&nbsp;
-          <td>
-                  <Card style={{ width: '13rem',backgroundColor:"grey",borderRadius:"10%"}}>
-                    <Card.Body><br/>
-                    <Card.Title className="carfont">Qualification</Card.Title>
-                    <Card.Text>
-                      MCA from Amirta School of arts and sciences kochi.
+                      </p>
+                  </div>
+                </div>
+
+                <div className="column">
+                <div className="card">
+                    <h6 className="carfont">Qualification</h6>
+                    <p>
+                    MCA from Amirta School of arts and sciences kochi.
                       [Interset and passion is more then a Degree..]
-                       
-                </Card.Text>
-                <Card.Link href="#" style={{fontSize:"17px", color:"white"}}>Loading...</Card.Link>
-                </Card.Body>
-                </Card></td>
-                <td>
-                <Card style={{ width: '13rem',backgroundColor:"grey",borderRadius:"10%"}}>
-                    <Card.Body><br/>
-                    <Card.Title className="carfont">walk of life</Card.Title>
-                    <Card.Text>
-                       <ul>
+                      </p>
+                  </div>
+                </div>
+                <div className="column">
+                <div className="card">
+                    <h6 className="carfont">walk of life</h6>
+                    <p>
+                    <ul>
                          <li>Mechine Learning </li>
                          <li>Web developing</li>
                          <li>Art works</li>
                          <li>spread love</li>
                        </ul>
-                </Card.Text>
-                <Card.Link href="#" style={{fontSize:"17px", color:"white"}}>Reloading...</Card.Link>
-                </Card.Body>
-                </Card></td>
-        </tr>
-      </table>
-
-
-
+                      </p>
+                  </div>
+                
+                </div>
+                </div>
+              
       </div>
       </Container>
-      <p style={{fontFamily:"sans-serif",fontSize:"10px"}}>send feedback / rjamaltdt@gmail.com</p>
     </div>
   );
 }
